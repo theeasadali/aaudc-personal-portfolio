374b5a38d96433e8e39430b5300fa4a4b14c769e
Asad Ali <theeasadali@gmail.com>
2024-11-10 03:52:00 +0500

    Completed Designing Footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,9 @@ export default function Footer() {
         <footer>
             <div className="container">
                 <div className="footer-info">
-                <Link href={"/"}><Image src={"/AUDC\ LOGO.svg"} height={40} width={100} alt="Asad's Unique Design & Code"/></Link>
+                <Link href={"/"}><Image src={"/AUDC\ LOGO\ Light.svg"} height={40} width={100} alt="Asad's Unique Design & Code"/></Link>
+                <Link href={"tel:+923053174570"}>+92 305 3174570</Link>
+                <Link href={"mailto:theeasadali@gmail.com"}>theeasadali@gmail.com</Link>
                 </div>
                 <div className="footer-links">
                     <p>Explore</p>
@@ -13,8 +15,8 @@ export default function Footer() {
                     <Link href={"/contact-me"}>Contact Me</Link>
                     <Link href={"/free-tools"}>Free Tools</Link>
                 </div>
-            <p>&copy;copyright AUDC 2024-25. All right reserved.</p>
             </div>
+            <p>&copy;copyright AUDC 2024-25. All right reserved.</p>
         </footer>
     )
 }

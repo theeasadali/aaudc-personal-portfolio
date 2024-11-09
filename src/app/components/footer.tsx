4bacb37c6ab66949f2593d79af04c641dba13255
Asad Ali <theeasadali@gmail.com>
2024-11-10 03:33:37 +0500

    Designed footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Footer() {
     return (
         <footer>
             <div className="container">
-                <p>&copy;copyright AUDC 2024-25. All right reserved.</p>
+                <div className="footer-info">
+                <Link href={"/"}><Image src={"/AUDC\ LOGO.svg"} height={40} width={100} alt="Asad's Unique Design & Code"/></Link>
+                </div>
                 <div className="footer-links">
+                    <p>Explore</p>
                     <Link href={"/about-me"}>About Me</Link>
                     <Link href={"/contact-me"}>Contact Me</Link>
                     <Link href={"/free-tools"}>Free Tools</Link>
                 </div>
+            <p>&copy;copyright AUDC 2024-25. All right reserved.</p>
             </div>
         </footer>
     )
